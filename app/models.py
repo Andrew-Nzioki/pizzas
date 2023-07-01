5cef8567148bcf03baaee8729f0c557ad88b6ab6
Andrew Kyalo Nzioki <andrewkyalonzioki001.com>
2023-07-01 14:23:36 +0300

    pizzas complete

--- app/models.py
+++ app/models.py
@@ -3,48 +3,48 @@ from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Restaurant(db.Model,SerializerMixin):
+class Restaurant(db.Model, SerializerMixin):
     __tablename__ = 'restaurants'
     
     serialize_rules = ('-pizzas.restaurant',)
 
     id = db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String)
-    address=db.Column(db.String)
-#one to many (One restaurant has many Pizzas)
-    pizzas=db.relationship('Pizza',backref='restaurant')
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+    # One to many (One restaurant has many Pizzas)
+    pizzas = db.relationship('Pizza', back_populates='restaurant')
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='restaurant')
 
-# add any models you may need. 
-class Pizza(db.Model,SerializerMixin):
+class Pizza(db.Model, SerializerMixin):
+    __tablename__ = 'pizzas'
+    serialize_rules = ('-restaurant_pizzas.pizza',)
 
-    __tablename__='pizzas'
-    serialize_rules = ('-restaurants_pizzas.pizza',)
-
-    id=db.Column(db.Integer,primary_key=True)
-    name=db.Column(db.String)
-    ingredients=db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    #pizzas relationship (One to many ,one pizza has many varieties)
-    restaurants_pizzas=db.relationship('RestaurantPizza',backref='Pizza')
+    # Define the foreign key
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
 
-class RestaurantPizza(db.Model,SerializerMixin):
+    # Pizzas relationship (One to many, one pizza has many varieties)
+    restaurant = db.relationship('Restaurant', back_populates='pizzas')
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='pizza')
 
-    __tablename__='restaurant_pizzas'
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = 'restaurant_pizzas'
 
-    serialize_rules = ('-pizza.restaurant_pizzas','-restaurant.restaurant_pizzas')
+    serialize_rules = ('-pizza.restaurant_pizzas', '-restaurant.restaurant_pizzas')
 
-    id=db.Column(db.Integer,primary_key=True)
-    price=db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    #relationships
-
+    # Relationships
     pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
 
-    #extrea step
-    pizza = db.relationship('Pizza', backref='restaurant_pizzas')
-    restaurant = db.relationship('Restaurant', backref='restaurant_pizzas')
+    pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
+    restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas')
--- app/seed.py
+++ app/seed.py
@@ -1,57 +1,30 @@
-#!/usr/bin/env python3
-
-from random import randint, choice as rc
-
+from app import app, db
+from models import Restaurant, Pizza, RestaurantPizza
 from faker import Faker
-
-from app import app
-from models import db, Pizza,RestaurantPizza,Restaurant
-
-
-
 
 fake = Faker()
 
-with app.app_context():
+def seed_data():
+    with app.app_context():
+        db.create_all()
 
-    Restaurant.query.delete()
-    Pizza.query.delete()
-    RestaurantPizza.query.delete()
+        # Create Restaurants
+        restaurants = []
+        for _ in range(5):
+            restaurant = Restaurant(name=fake.company(), address=fake.address())
+            restaurants.append(restaurant)
+            db.session.add(restaurant)
+        db.session.commit()
 
-    restaurants = []
-    for i in range(20):
-        u = Restaurant(name=fake.name(),
-                  address=fake.address(),)
-        restaurants.append(u)
+        # Create Pizzas and RestaurantPizzas
+        for restaurant in restaurants:
+            for _ in range(3):
+                pizza = Pizza(name=fake.word(), ingredients=fake.word(), restaurant=restaurant)
+                db.session.add(pizza)
+                db.session.commit()
+                restaurant_pizza = RestaurantPizza(restaurant=restaurant, pizza=pizza, price=fake.random_int(min=5, max=20))
+                db.session.add(restaurant_pizza)
+        db.session.commit()
 
-    db.session.add_all(restaurants)
-
-    pizzas = []
-    for i in range(100):
-        g =   Pizza(
-            name=fake.sentence(),
-            ingredients=fake.name(),
-           
-            price=randint(5, 60),
-        )
-        pizzas.append(g)
-
-    db.session.add_all(pizzas)
-
-    restaurants_pizzas = []
-    for u in restaurants_pizzas:
-        for i in range(randint(1, 10)):
-
-            r = RestaurantPizza(
-                price=randint(5,60),
-                )
-            restaurants_pizzas.append(r)
-
-    db.session.add_all(restaurants_pizzas)
-
-    # for g in games:
-    #     r = rc(reviews)
-    #     g.review = r
-    #     reviews.remove(r)
-
-    # db.session.commit()
+if __name__ == '__main__':
+    seed_data()
